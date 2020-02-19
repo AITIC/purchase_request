6b80c709e7acbfa7dc38fc95cfa1a816ba9f60b6
101t <tarek.it.eng@gmail.com>
2020-02-19 12:13:16 +0300

    fixing manifest

--- boraq_purchase_request/__manifest__.py
+++ boraq_purchase_request/__manifest__.py
@@ -2,23 +2,22 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Purchase Request1',
-    'version': '1.2',
+    'name': 'Purchase Request',
+    'version': '13.0.1.0.0',
     'category': 'Purchases',
-    'sequence': 60,
-    'summary': 'Purchase orders, tenders and agreements',
+    'summary': 'Purchase Request is an alternative and simple module to Approvals',
     'description': "",
-    'website': 'https://www.odoo.com/page/purchase',
+    'author': "Boraq-Group",
+    'website': 'https://boraq-group.com',
     'depends': ['account','purchase'],
+    "license": "AGPL-3",
     'data': [
         'views/requster.xml',
         'reports/report.xml',
         'reports/report_card.xml',
         'security/ir.model.access.csv', 
     ],
-    # 'demo': [
-    #     'data/purchase_demo.xml',
-    # ],
+    'demo': [],
     'installable': True,
     'auto_install': False,
     'application': True,
