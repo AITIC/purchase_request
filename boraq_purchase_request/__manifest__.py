b1a6bfdc275f6b420ee6d34d92399282f7ed751e
Ammar Almarawi <ammarm.boraq-group@gmail.com>
2020-03-02 20:51:32 +0300

    remove the reports

--- boraq_purchase_request/__manifest__.py
+++ boraq_purchase_request/__manifest__.py
@@ -15,8 +15,6 @@
         'security/security.xml', 
         'security/ir.model.access.csv', 
         'views/requester.xml',
-        'reports/report.xml',
-        'reports/report_card.xml',
     ],
     "images": ['static/description/banner.png'],
     'demo': [
